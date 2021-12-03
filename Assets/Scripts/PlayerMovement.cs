b59c8406240cd76af0fc0f32bcaa83e472dfb7ee
Kieran McAuliffe <bkieranmcauliffe@gmail.com>
2021-12-03 17:30:31 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public GameObject dropPos;
+    public GameObject[] dropObs;
+    private int selectedOb = 0;
+
+    public float speed = 12;
+    Vector3 velocity;
+    bool isGrounded;
+    public float gravity = -9.81f;
+
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateMaterial();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Click");
+            Instantiate(dropObs[selectedOb], dropPos.transform.position, Quaternion.identity);
+            foreach (GameObject spirit in GameObject.FindGameObjectsWithTag("Spirit"))
+            {
+                SpiritController spCon = spirit.GetComponent<SpiritController>();
+                spCon.UpdateTarget();
+            }
+        }
+
+        if (Input.GetKeyDown("q"))
+        {
+            selectedOb = (selectedOb - 1);
+            if (selectedOb == -1)
+            {
+                selectedOb = dropObs.Length - 1;
+            }
+            UpdateMaterial();
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            selectedOb = (selectedOb + 1) % dropObs.Length;
+            UpdateMaterial();
+        }
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void UpdateMaterial()
+    {
+        dropPos.GetComponent<MeshRenderer>().material = dropObs[selectedOb].GetComponent<MeshRenderer>().sharedMaterial;
+        dropPos.transform.localScale = dropObs[selectedOb].transform.localScale;
+    }
+}
---
+++ Assets/Scripts/SpiritController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritController : MonoBehaviour
+{
+
+    private int targetNum = 0;
+    private GameObject[] targets = { };
+    public float moveSize = .5f;
+    private bool backwards;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backwards = (Random.value > 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targets.Length > 1)
+        {
+            Vector3 targetPos = targets[targetNum].transform.position;
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSize);
+
+            if (Vector3.Distance(transform.position, targetPos) < moveSize)
+            {
+                UpdateTarget();
+            }
+        }
+    }
+
+    public void UpdateTarget()
+    {
+        targets = GameObject.FindGameObjectsWithTag("Orb");
+        if (!backwards)
+        {
+            targetNum = (targetNum + 1) % targets.Length;
+        }
+        else
+        {
+            targetNum -= 1;
+            if (targetNum == 0)
+            {
+                targetNum = targets.Length - 1;
+            }
+        }
+        
+    }
+}
